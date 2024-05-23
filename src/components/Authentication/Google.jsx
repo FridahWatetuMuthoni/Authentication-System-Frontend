68677080b2086ce05400926fb4deaf058862d7ff
FridahWatetu <fridahwatetumuthoni@gmail.com>
2024-05-23 10:14:16 +0300

    added the social authentication google and facebook

--- src/components/Authentication/Google.jsx
+++ src/components/Authentication/Google.jsx
@@ -1,16 +1,44 @@
 import { LoginSocialGoogle } from "reactjs-social-login";
 import { GoogleLoginButton } from "react-social-login-buttons";
+import useGlobalContext from "../../hooks/useGlobalContext";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../api/axiosInstance";
 
 function Google() {
   const clientID =
     "1085618487417-27lqrukr7qc7mg5vng7upj9er8ksjbe7.apps.googleusercontent.com";
+  const navigate = useNavigate();
+  const { setAccessToken, setRefreshToken, setUserID } = useGlobalContext();
+
+  const mutation = useMutation({
+    mutationFn: async (payload) => {
+      const response = await axiosInstance.post("google/", payload);
+      if (response.status === 200) {
+        console.log(response);
+        setRefreshToken(response?.data?.refresh);
+        setAccessToken(response?.data?.access);
+        localStorage.setItem("access_token", response?.data?.access);
+        localStorage.setItem("refresh_token", response?.data?.refresh);
+        localStorage.setItem("userID", response?.data.user.pk);
+        setUserID(response?.data.user.pk);
+        navigate("/");
+      }
+      return response;
+    },
+  });
 
   const handleReject = (error) => {
     console.log(error);
   };
   const handleSuccess = (response) => {
-    console.log(response);
-    console.log(response.data);
+    const access = response?.data?.access_token;
+    if (access) {
+      const payload = {
+        access_token: access,
+      };
+      mutation.mutateAsync(payload);
+    }
   };
   return (
     <div className="w-full">
