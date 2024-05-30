da81067068117a51ec99d791736745a1f601214c
FridahWatetu <fridahwatetumuthoni@gmail.com>
2024-05-30 13:37:36 +0300

    refactor

--- src/hooks/useGlobalContext.jsx
+++ src/hooks/useGlobalContext.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 
+//global context
 function useGlobalContext() {
   return useContext(GlobalContext);
 }
